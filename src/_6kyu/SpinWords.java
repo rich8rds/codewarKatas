5b00962dbb2eb3dcbfc70fe52d6c432907cdf65a
Decagon <decagon@Decagons-MacBook-Air.local>
2022-09-27 11:00:15 +0100

    SpiWords kata

--- src/_6kyu/SpinWords.java
+++ src/_6kyu/SpinWords.java
@@ -1,3 +1,4 @@
+package _6kyu;
 //Stop gninnipS My sdroW!
 //Write a function that takes in a string of one or more words, and returns the same string,
 //but with all five or more letter words reversed (Just like the name of this Kata).
